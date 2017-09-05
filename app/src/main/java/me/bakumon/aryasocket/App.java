2ee25d41b4546eeac7c58682b2d345b69e9cc5fd
Bakumon <bakumon@aliyun.com>
2017-09-05 16:55:13 +0800

    perfect the first version

--- app/src/main/java/me/bakumon/aryasocket/App.java
+++ app/src/main/java/me/bakumon/aryasocket/App.java
@@ -15,24 +15,10 @@ public class App extends Application {
     public void onCreate() {
         super.onCreate();
 
-        String hear = "{" +
-                "\"inname\":\"heartbeat\",\"data\":{" +
-                "\"uid\":" + "\"" + "0" + "\"" + "," +
-                "\"sys\":" + "\"" + "Android" + "\"" + "," +
-                "\"sysver\":" + "\"" + "unkown" + "\"" + "," +
-                "\"ver\":" + "\"" + "1.0.0" + "\"" + "," +
-                "\"devid\":" + "\"" + "unkown" + "\"" + "," +
-                "\"platform\":" + "\"" + "unkown" + "\"" + "," +
-                "\"terminal\":" + "\"Cheji\"" + "," +
-                "\"city\":" + "\"" + "北京" + "\"" + "," +
-                "\"oid\":" + "\"" + "0" + "\"" + "," +
-                "\"secret\":" + "\"" + "3ccd8bfc139d10658c2ac38823fae960" + "\"" + "," +
-                "\"type\": 1" +
-                "}" +
-                "}";
+        String heartbeat = "";
 
-        AryaConfig aryaConfig = new AryaConfig.Builder("http://dev.banyar.cn:9443/")
-                .setHeartData(hear).build();
+        AryaConfig aryaConfig = new AryaConfig.Builder("")
+                .setHeartData(heartbeat).build();
 
         AryaSocket.getInstance().init(this, aryaConfig);
     }
